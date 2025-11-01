926d278c7afc78be4d226d2888e1531d9ff92393
Slick User <azyaz005@gmail.com>
2025-11-01 23:23:42 +0500

    vercel hosting?

--- server/server.js
+++ server/server.js
@@ -26,5 +26,6 @@ app.use("/", routes);
 
 // Server Listen
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`Server running on port ${PORT}`));
+//app.listen(5000, () => console.log(`Server running on port ${PORT}`));
 
+module.exports = app;
